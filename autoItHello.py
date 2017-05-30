226abd48d33cb2419dd0ed65d0e52c3c1f1edc00
Stéphane Beaudet <stephane.beaudet@csharricana.qc.ca>
2017-05-30 13:48:48 -0400

    ??

--- autoItHello.py
+++ autoItHello.py
@@ -2,6 +2,7 @@ import autoit #pip install -U pyautoit
 import easygui #pip install -U easygui
 from tinydb import TinyDB, Query #pip install -U tonydb
 from datetime import datetime
+import xlwt
 '''
 #AutoIt Hello World
 autoit.run("notepad.exe")
@@ -53,9 +54,48 @@ def fetchItemsData():
         itemPrice = easygui.enterbox("Entrez le prix le plus bas pour la vente de '" + singleItem["name"] + "'", "Entrée du prix", "")
         data.insert({'name': singleItem["name"], 'price': itemPrice, 'timestamp': str(datetime.now())})
 
+def dateReString(original):
+        elementdateTime =  datetime.strptime(original, "%Y-%m-%d %H:%M:%S.%f")
+        dateString = elementdateTime.strftime("%Y-%m-%d")
+        return dateString
+
+def excelExport():
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet('A Test Sheet')
+    #ws.write(2, 1, 1)
+    #ws.write(2, 2, xlwt.Formula("A3+B3"))
+    DatesQuery = Query()
+    datesResult = data.all()
+    distinctDates = []
+    for oneData in datesResult:    
+        date = dateReString(oneData["timestamp"])
+        if date not in distinctDates
+            distinctDates.append(date)
+        
+    i=0
+    for oneDate in distinctDates:
+        i=i+1
+
+        '''
+        allItems = items.all()
+        for oneItem in allItems:
+            #titre des colonnes
+            ws.write(0, i+1, oneItem["name"])
+            
+            ItemQuery = Query()
+            dataResult = data.search(ItemQuery.name == oneItem["name"])
+            j=1
+            for oneData in dataResult:
+                #ws.write(j, 0, oneData["timestamp"])
+                ws.write(j, i+1, int(oneData["price"]))
+                j=j+1
+            '''
+    wb.save('example.xls')
+        
 
 
-menuChoice=["1 - Ajouter un item à suivre", "2 - Afficher la liste des items à suivre", "3 - Effectuer le suivi des items"]
+
+menuChoice=["1 - Ajouter un item à suivre", "2 - Afficher la liste des items à suivre", "3 - Effectuer le suivi des items", "5 - Exporter vers Excel"]
 choice = easygui.choicebox("Que voulez-vous faire?", "Logiciel Awesome", menuChoice)
 
 choiceNum = choice[:1]
@@ -70,5 +110,8 @@ elif int(choiceNum) == 2:
 elif int(choiceNum) == 3:
     print "Suivi des items"
     fetchItemsData()
+elif int(choiceNum) == 5:
+    print "Exportation Excel"
+    excelExport()
 else:
     print "Aucun Choix"
